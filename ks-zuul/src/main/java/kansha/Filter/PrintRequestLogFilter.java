5c4f23fa1355179e1db97d502b95c7f4583efc97
SLSIC <slsic_m@163.com>
2019-08-02 18:00:59 +0800

    创建新项目

--- ks-zuul/src/main/java/kansha/Filter/PrintRequestLogFilter.java
+++ ks-zuul/src/main/java/kansha/Filter/PrintRequestLogFilter.java
@@ -24,7 +24,7 @@ public class PrintRequestLogFilter extends ZuulFilter {
 
     @Override
     public String filterType() {
-        return FilterConstants.POST_TYPE;//要打印返回信息，必须得用"post"
+        return FilterConstants.PRE_TYPE;//要打印返回信息，必须得用"post"
     }
 
     @Override
@@ -39,49 +39,49 @@ public class PrintRequestLogFilter extends ZuulFilter {
 
     @Override
     public Object run() {
-        try {
-            RequestContext ctx = RequestContext.getCurrentContext();
-            HttpServletRequest request = ctx.getRequest();
-            InputStream in = request.getInputStream();
-            String reqBbody = StreamUtils.copyToString(in, Charset.forName("UTF-8"));
-            // 打印userId，获取其他用户信息
-            if (reqBbody != null) {
-                JSONObject json = JSONObject.fromObject(reqBbody);
-                Object userId = json.get("userId");
-                if (userId != null) {
-                    log.info("request userId:\t" + userId);
-                }
-            }
-            // 打印请求方法，路径
-            log.info("request url:\t" + request.getMethod() + "\t" + request.getRequestURL().toString());
-            Map<String, String[]> map = request.getParameterMap();
-            // 打印请求url参数
-            if (map != null) {
-                StringBuilder sb = new StringBuilder();
-                sb.append("request parameters:\t");
-                for (Map.Entry<String, String[]> entry : map.entrySet()) {
-                    sb.append("[" + entry.getKey() + "=" + printArray(entry.getValue()) + "]");
-                }
-                log.info(sb.toString());
-            }
-            // 打印请求json参数
-            if (reqBbody != null) {
-                log.info("request body:\t" + reqBbody);
-            }
-
-            // 打印response
-            InputStream out = ctx.getResponseDataStream();
-            String outBody = StreamUtils.copyToString(out, Charset.forName("UTF-8"));
-            if (outBody != null) {
-                log.info("response body:\t" + outBody);
-            }
-
-            ctx.setResponseBody(outBody);//重要！！！
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        log.info("---------------------------------------");
+//        try {
+//            RequestContext ctx = RequestContext.getCurrentContext();
+//            HttpServletRequest request = ctx.getRequest();
+//            InputStream in = request.getInputStream();
+//            String reqBbody = StreamUtils.copyToString(in, Charset.forName("UTF-8"));
+//            // 打印userId，获取其他用户信息
+//            if (reqBbody != null) {
+//                JSONObject json = JSONObject.fromObject(reqBbody);
+//                Object userId = json.get("userId");
+//                if (userId != null) {
+//                    log.info("request userId:\t" + userId);
+//                }
+//            }
+//            // 打印请求方法，路径
+//            log.info("request url:\t" + request.getMethod() + "\t" + request.getRequestURL().toString());
+//            Map<String, String[]> map = request.getParameterMap();
+//            // 打印请求url参数
+//            if (map != null) {
+//                StringBuilder sb = new StringBuilder();
+//                sb.append("request parameters:\t");
+//                for (Map.Entry<String, String[]> entry : map.entrySet()) {
+//                    sb.append("[" + entry.getKey() + "=" + printArray(entry.getValue()) + "]");
+//                }
+//                log.info(sb.toString());
+//            }
+//            // 打印请求json参数
+//            if (reqBbody != null) {
+//                log.info("request body:\t" + reqBbody);
+//            }
+//
+//            // 打印response
+//            InputStream out = ctx.getResponseDataStream();
+//            String outBody = StreamUtils.copyToString(out, Charset.forName("UTF-8"));
+//            if (outBody != null) {
+//                log.info("response body:\t" + outBody);
+//            }
+//
+//            ctx.setResponseBody(outBody);//重要！！！
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
         return null;
     }
 
